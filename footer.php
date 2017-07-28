0c4c3304534b69f83d3fb5f759d4bc02322df7e4
Louis Dalligos <zindex14@gmail.com>
2017-07-28 20:32:11 +0800

    Add footer, widget custom fields

--- footer.php
+++ footer.php
@@ -11,6 +11,75 @@
 
 ?>
 
+  <footer>
+  	<div class="skoda1_widget-area">
+  		<div class="container">
+  			<div class="skoda1_widget-overlay-bg">
+  				<div class="col-md-6 col-sm-6 column-widget-1">
+  					<div class="row">
+  						<div class="col-xs-6 col-sm-6 col-md-6 address-widget">
+  							<div class="widget-inner">
+  								<i class="fa fa-map-marker"></i>
+  								<address>
+  									<?php echo get_theme_mod('site_address'); ?>
+  								</address>
+  							</div>
+  						</div>
+  						<div class="col-xs-6 col-sm-6 col-md-6 phone-widget">
+  							<div class="widget-inner">
+  								<i class="fa fa-phone"></i>
+                  <span><?php echo get_theme_mod('site_contact_number'); ?></span>
+  							</div>
+  						</div>
+  					</div>
+            <!-- Add the extra clearfix for only the required viewport -->
+  					<div class="row">
+  						<div class="col-xs-6 col-sm-6 col-md-6 email-widget">
+  							<div class="widget-inner">
+  								<i class="fa fa-envelope"></i>
+                  <a class="visible-md visible-lg" href="mailto:<?php echo get_theme_mod('site_email'); ?>"><?php echo get_theme_mod('site_email'); ?></a>
+                  <a class="visible-xs visible-sm" href="<?php echo get_theme_mod('site_email'); ?>">Email Us</a>
+  							</div>
+  						</div>
+  						<div class="col-xs-6 col-sm-6 col-md-6 schedule-widget">
+  							<div class="widget-inner">
+  								<i class="fa fa-check-circle-o"></i>
+                  <?php echo get_theme_mod('site_schedule'); ?>
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  				<div class="col-md-6 col-sm-6 column-widget-2 hidden-xs">
+  					<h3 class="skoda1_widget-title">About Us</h3>
+  					<?php echo get_theme_mod('site_about_desc'); ?>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  	<div class="skoda1_footer-bottom container">
+  		<div class="row">
+  			<div class="col-md-9 col-sm-9">
+  				<p class="skoda1-copyright">&copy; 2015 <?php bloginfo( 'name' ); ?></p>
+          <?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-2',
+							'menu_id'        => 'footer-menu',
+							'container'      => 'p',
+							'menu_class' => 'footer-menu'
+						) );
+					?>
+  				<p class="dealer_link"><a href="#">Dealer Website</a> by Dealer Solutions</p>
+  			</div>
+  			<div class="col-md-3 col-sm-3 skoda1_footer-social-links-wrap">
+  				<div class="skoda1_footer-social">
+  					<a href="<?php echo get_theme_mod('facebook_link'); ?>"><i class="fa fa-facebook"></i></a>
+            <a href="<?php echo get_theme_mod('twitter_link'); ?>"><i class="fa fa-twitter"></i></a>
+            <a href="<?php echo get_theme_mod('youtube_link'); ?>"><i class="fa fa-youtube"></i></a>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  </footer>
 
 <?php wp_footer(); ?>
 
--- inc/customizer.php
+++ inc/customizer.php
@@ -25,6 +25,141 @@ function ds_skoda_customize_register( $wp_customize ) {
 			'render_callback' => 'ds_skoda_customize_partial_blogdescription',
 		) );
 	}
+
+
+	// sections
+	$wp_customize->add_section('footer_widgets_section', array(
+		'title' => __("Footer Widgets", 'ds-skoda')
+	));
+
+	$wp_customize->add_section('footer_social_media_links', array(
+		'title' => __("Social Media Links", 'ds-skoda')
+	));
+
+	// settings
+	$wp_customize->add_setting('site_address', array(
+		'default' => __("Add your address here"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('site_contact_number', array(
+		'default' => __("Add your contact number here"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('site_email', array(
+		'default' => __("Add your email here"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('site_schedule', array(
+		'default' => __("Add your business schedule"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('site_about_desc', array(
+		'default' => __("Add content of about section"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('facebook_link', array(
+		'default' => __("Add your facebook profile"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('twitter_link', array(
+		'default' => __("Add your twitter profile"),
+		'transport' => 'refresh'
+	));
+
+	$wp_customize->add_setting('youtube_link', array(
+		'default' => __("Add your youtube profile"),
+		'transport' => 'refresh'
+	));
+
+
+	// control
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_address',
+		array(
+			'label' => __("Address", 'ds-skoda'),
+			'section' => 'footer_widgets_section',
+			'settings' => 'site_address'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_contact_number',
+		array(
+			'label' => __("Contact Number", 'ds-skoda'),
+			'section' => 'footer_widgets_section',
+			'settings' => 'site_contact_number'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_email',
+		array(
+			'label' => __("Email", 'ds-skoda'),
+			'section' => 'footer_widgets_section',
+			'settings' => 'site_email'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_schedule',
+		array(
+			'label' => __("Business Schedule", 'ds-skoda'),
+			'section' => 'footer_widgets_section',
+			'settings' => 'site_schedule',
+			'type' => 'textarea'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_about',
+		array(
+			'label' => __("About Description", 'ds-skoda'),
+			'section' => 'footer_widgets_section',
+			'settings' => 'site_about_desc',
+			'type' => 'textarea'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_fb_link',
+		array(
+			'label' => __("Facebook Profile URL", 'ds-skoda'),
+			'section' => 'footer_social_media_links',
+			'settings' => 'facebook_link'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_twitter_link',
+		array(
+			'label' => __("Twitter Profile URL", 'ds-skoda'),
+			'section' => 'footer_social_media_links',
+			'settings' => 'twitter_link'
+		)
+	));
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'theme_footer_youtube_link',
+		array(
+			'label' => __("Youtube Profile URL", 'ds-skoda'),
+			'section' => 'footer_social_media_links',
+			'settings' => 'youtube_link'
+		)
+	));
 }
 add_action( 'customize_register', 'ds_skoda_customize_register' );
 
